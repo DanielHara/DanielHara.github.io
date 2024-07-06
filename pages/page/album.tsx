98e63e7b78d6b7aaf4d0d6a83634e36ed86ba056
Daniel Hara <haradaniel734@gmail.com>
2024-07-06 18:02:47 +0200

    create album page

---
+++ pages/page/album.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Header from '../../components/Header';
+
+function Album() {
+  return (
+    <>
+      <Header tag='hidden' title='About me' />
+      Foto album here
+    </>
+  );
+}
+
+export default Album;
