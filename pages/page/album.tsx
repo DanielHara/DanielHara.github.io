d42ef7db43768ecd5bf3d084f0e37212ca963110
Daniel Hara <haradaniel734@gmail.com>
2024-07-06 19:23:13 +0200

    create album infinite scroll

--- pages/page/album.tsx
+++ pages/page/album.tsx
@@ -1,13 +1,11 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import Header from '../../components/Header';
 import ViewAlbum from '../../views/Album';
-import { getPictureLoad, IImage } from '../../api/getPictures';
-
 
 function Album() {
   return (
     <>
-      <Header tag='hidden' title='About me' />
+      <Header tag='hidden' title='Travel photo gallery' />
       <ViewAlbum />
     </>
   );
--- views/Album.tsx
+++ views/Album.tsx
@@ -4,26 +4,34 @@ import Image from 'next/image'
 
 import React, { useEffect, useState } from 'react';
 import { getPictureLoad, IImage } from '../api/getPictures';
+import { useInView } from 'react-intersection-observer';
 
 
 const ViewAlbum = () => {
     // TO-DO: server render the first pictures
     const [pictures, setPictures] = useState<IImage[]>([]);
+    const [isLoading, setIsLoading] = useState(false);
+
+    const { ref: endOfListRef, inView } = useInView();
 
     useEffect(() => {
-      getPictureLoad()
-          .then(loadedPictures => setPictures(loadedPictures))
-          .catch(() => { console.log('Issue fetching images ')});
-    }, []);
+        if (inView && !isLoading) {
+            setIsLoading(true);
+            getPictureLoad()
+                .then(loadedPictures => setPictures((previousPictures) => previousPictures.concat(loadedPictures)))
+                .then(() => { setIsLoading(false) })
+                .catch(() => { console.log('Issue fetching images ')});
+        }
+      }, [inView]);
   
     return (
       <div>
-        Just an album here
         <Grid container spacing={2}>
             {pictures.map((picture) => <Grid item xs={12} md={4} height='300px' position="relative">
                 <Image src={picture.url} fill style={{ objectFit: 'contain' }}/>
             </Grid>)}
         </Grid>
+        <div ref={endOfListRef}>You reached the end of the page</div>
       </div>
     );
 }
